2594268af6cd61a20cb54c7a1253ce54f7b51935
Burak Orkmez <burakorkmez@Buraks-MacBook-Pro.local>
2024-12-30 19:28:50 +0300

    project completed

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import DesktopNavbar from "./DesktopNavbar";
+import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
+
+async function Navbar() {
+  const user = await currentUser();
+  if (user) await syncUser(); // POST
+
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
+              Socially
+            </Link>
+          </div>
+
+          <DesktopNavbar />
+          <MobileNavbar />
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
